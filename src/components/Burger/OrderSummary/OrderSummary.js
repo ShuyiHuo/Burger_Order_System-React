3c6e50f4b4055a414029aaf1d8c7be991dd453fe
ShuyiHuo <huoshuyi1995@gmail.com>
2020-10-16 16:21:26 -0400

    change const to class in ordersummary and model

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const ingredoentSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    componentWillUpdate() {
+        console.log("ordersummary will update");
+    }
+    render () {
+        const ingredoentSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key = {igKey}>
-                    <span style = {{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style = {{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A dilicious burger with the following ingredients:</p>
-            <ul>
-                {ingredoentSummary}
-            </ul>
-            <p><strong>Total Price:{props.price.toFixed(2)}</strong></p>
-            <p>Continue to check out?</p>
-            <Button btnType = 'Danger' clicked = {props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType = 'Success' clicked = {props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
-export default orderSummary;
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A dilicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredoentSummary}
+                </ul>
+                <p><strong>Total Price:{this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to check out?</p>
+                <Button btnType = 'Danger' clicked = {this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType = 'Success' clicked = {this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+export default OrderSummary;
--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Model.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const model = (props) => (
-    <Aux>
-        <Backdrop show = {props.show} clicked = {props.modelClosed}/>
-        <div 
-            className = {classes.Modal}
-            style = {{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}> 
-            {props.children}
-        </div>
+class Model extends Component {
+    shouldComponentUpdate(nextProps,nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-    </Aux>
-    
-);
+    componentWillUpdate () {
+        console.log("Model will update");
+    }
+    render () {
+        return (
+            <Aux>
+                <Backdrop show = {this.props.show} clicked = {this.props.modelClosed}/>
+                <div 
+                    className = {classes.Modal}
+                    style = {{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}> 
+                    {this.props.children}
+                </div>
 
-export default model;
+            </Aux>
+        );
+    }
+} 
+
+export default Model;
