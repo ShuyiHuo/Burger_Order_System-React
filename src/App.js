7c6ce2c6a6be10b0da8cc5473d8f938481a4d5fd
ShuyiHuo <huoshuyi1995@gmail.com>
2020-10-26 15:51:34 -0400

    10.26

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React ,{Component} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import {Route, Switch} from 'react-router-dom';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
 class App extends Component {
   
   render(){
@@ -8,7 +12,11 @@ class App extends Component {
     return (
       <div >
         <Layout>
-           <BurgerBuilder/> 
+          <Switch>
+            <Route path = "/checkout" component ={Checkout}/>
+            <Route path = "/orders" component ={Orders}/>
+            <Route path = "/" exact component ={BurgerBuilder}/>
+          </Switch>           
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,29 +89,40 @@ class SaladBuilder extends Component {
     purchaseContinueHandler = () => {
         //add url here
         //alert("you countinue!");
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Zoe',
-                address:{
-                    street: '111',
-                    zipcode: '02215',
-                    country: 'USA'
-                },
-                email: 'xxx@gmail.com'
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Zoe',
+        //         address:{
+        //             street: '111',
+        //             zipcode: '02215',
+        //             country: 'USA'
+        //         },
+        //         email: 'xxx@gmail.com'
 
-            },
-            deliverMethod: 'to go'
+        //     },
+        //     deliverMethod: 'to go'
+        // }
+        // axios.post('/orders.json',order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false,purchasing: false});
+        //     });
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json',order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false,purchasing: false});
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
 
     }
 
